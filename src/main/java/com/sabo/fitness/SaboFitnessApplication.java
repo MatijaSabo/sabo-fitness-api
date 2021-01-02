fc3fdcdefc889942d815c6bc43c6992595511428
Matija Sabolić <matija.sabolic@ecx.io>
2021-01-02 21:04:15 +0100

    Initial commit

---
+++ src/main/java/com/sabo/fitness/SaboFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.sabo.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabo/fitness/SaboFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabo.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
